53661cfd63f28d4bdade00a01af31e34a5d25caf
Mahdia-toubal <mahdia.toubal@gmail.com>
2023-10-20 02:52:05 +0100

    Exo 3 Done Exo 2 Premier test

---
+++ TP1/Exo2/src/com/tests/ReservationDaoTest.java
@@ -0,0 +1,83 @@
+package com.tests;
+
+import com.example.dao.*;
+import com.example.entity.User;
+import com.example.entity.Parking;
+import com.example.entity.ParkingPlace;
+import com.example.entity.PlaceStatus;
+import com.example.entity.Reservation;
+import com.example.entity.ReservationStatus;
+import org.junit.Before;
+import org.junit.Test;
+import java.util.Date;
+
+import static org.junit.Assert.assertEquals;
+
+import java.sql.Connection;
+import java.util.Date;
+
+public class ReservationDaoTest {
+    private static DatabaseConnection DBconnection;
+    private static Connection connection;
+    private static ReservationDao reservationdao;
+
+    @Before
+    //pour créer une connexion vers la base de données.
+    public void init() {
+        DBconnection = new DatabaseConnection("sa", "", "org.h2.Driver", "jdbc:h2:mem:test");
+        connection = DBconnection.connect();
+        DBconnection.createDb(connection);
+
+        User user1=new User() ;//créer un nouveau utilisateur
+        user1.setUserId(1);
+        user1.setEmail("");
+        user1.setName("");
+        user1.setPhone("");
+
+        UserDao userDao = new UserDao();
+        userDao.setConn(connection);
+        userDao.insertUser(user1);
+
+
+        Parking Park=new Parking(); //créer un nouveau parking
+        Park.setName("");
+        Park.setAddress("");
+        Park.setParkingId(1);
+        Park.setCapacity(10);
+
+        ParkingDao parkingDao = new ParkingDao();
+        parkingDao.setConn(connection);
+        parkingDao.insertParking(Park);
+
+        ParkingPlace placeP = new ParkingPlace();//créer une nouvelle place parking
+        placeP.setIdPlace(1);
+        placeP.setPlaceName("");
+        PlaceStatus sta = PlaceStatus.values()[0];
+        placeP.setPlaceStatus(sta);
+        placeP.setParking(Park);
+
+
+        ParkingPlaceDao parkingPlaceDao = new ParkingPlaceDao();
+        parkingPlaceDao.setConn(connection);
+        parkingPlaceDao.insertParkingPlace(placeP);
+
+
+        Reservation Reservation1 = new Reservation();
+        Reservation1.setReservationId(1);
+        Reservation1.setStartTime(new Date(2023,10,13));
+        Reservation1.setEndTime(new Date(2023,10,16));
+        Reservation1.setParkingPlace(placeP);
+        Reservation1.setUser(user1);
+        ReservationStatus Rsta = ReservationStatus.values()[0];
+        Reservation1.setStatus(Rsta);
+
+        reservationdao = new ReservationDao();
+        reservationdao.setConn(connection);
+        reservationdao.insertReservation(Reservation1);
+
+    }
+    @Test
+    public void insertReservation() {
+        assertEquals(reservationdao.getReservationById(1).getUser().getUserId(),1);
+    }
+}
---
+++ TP1/Exo3/src/com/example/tests/UserServiceClientTest.java
@@ -0,0 +1,80 @@
+package com.example.tests;
+import com.example.model.*;
+import com.example.service.*;
+import org.junit.Before;
+import org.junit.Test;
+
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.net.http.HttpResponse;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+public class UserServiceClientTest {
+
+    private UserServiceClient userServiceClient;
+    private Endpoint mockEndpoint;
+    private final String githubApiUrl = "https://api.github.com/users/testuser";
+
+
+    @Test
+    public void getUserUnitTest() throws URISyntaxException, IOException, InterruptedException {
+        userServiceClient = new UserServiceClient();
+        mockEndpoint = mock(Endpoint.class);//create a mock for the endpoint
+        userServiceClient.setEndpoint(mockEndpoint);
+        String mockResponseBody = "{\"login\":\"testuser\",\"id\":1,\"node_id\":\"nodeid123\",\"avatar_url\":\"avatarurl\",\"type\":\"User\",\"name\":\"Test User\",\"company\":\"Test Company\",\"blog\":\"testblog.com\",\"location\":\"Test Location\",\"email\":\"test@test.com\",\"hireable\":true,\"bio\":\"Test Bio\",\"public_repos\":5,\"public_gists\":0,\"followers\":2,\"following\":1,\"created_at\":\"2023-10-01T12:00:00Z\",\"updated_at\":\"2023-10-10T12:00:00Z\"}";
+        HttpResponse<String> mockResponse = mock(HttpResponse.class); // ctreat an HttpsResponse mock
+        when(mockResponse.body()).thenReturn(mockResponseBody);
+        when(mockResponse.statusCode()).thenReturn(200);
+        when(mockEndpoint.getResponse(githubApiUrl)).thenReturn(mockResponse);
+
+        GitHubResponse expectedResponse = new GitHubResponse();
+        GitHubUser expectedUser = new GitHubUser();
+        expectedUser.setLogin("Login");
+        expectedUser.setId(1);
+        expectedUser.setNodeId("nodeid123");
+        expectedUser.setAvatarUrl("avatarurl");
+        expectedUser.setType("User");
+        expectedUser.setName("Test User");
+        expectedUser.setCompany("Test Company");
+        expectedUser.setBlog("testblog.com");
+        expectedUser.setLocation("Test Location");
+        expectedUser.setEmail("test@test.com");
+        expectedUser.setHireable(true);
+        expectedUser.setBio("Test Bio");
+        expectedUser.setPublicRepos(5);
+        expectedUser.setPublicGists(0);
+        expectedUser.setFollowers(2);
+        expectedUser.setFollowing(1);
+        expectedUser.setCreatedAt("2023-10-17");
+        expectedUser.setUpdatedAt("2023-10-19");
+
+        expectedResponse.setStatusCode(200);
+        expectedResponse.setGitHubUser(expectedUser);
+
+        GitHubResponse actualResponse = userServiceClient.getUser("https://api.github.com/users/testuser");
+
+        assertEquals(expectedResponse.getStatusCode(), actualResponse.getStatusCode());
+        assertEquals(expectedResponse.getGitHubUser().getName(), actualResponse.getGitHubUser().getName());
+
+    }
+
+        @Test
+        public void getUserIntegrationTest() throws URISyntaxException, IOException, InterruptedException {
+
+            userServiceClient = new UserServiceClient();
+            Endpoint endpoint = new Endpoint(); // integration = on a pas besoin des mocks
+            userServiceClient.setEndpoint(endpoint);
+            String validGitHubUserUrl = "https://api.github.com/users/etudiant";
+            GitHubResponse response = userServiceClient.getUser(validGitHubUserUrl);
+
+            assertNotNull(response);
+            assertNotNull(response.getGitHubUser());
+            assertEquals(200, response.getStatusCode());
+        }
+    }
+
+
